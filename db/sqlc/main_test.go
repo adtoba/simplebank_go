7cd112e8ac6b2d1008c0303c0a717201e92b37b8
adtoba <adetoba53@gmail.com>
2023-09-05 09:18:19 +0100

    chore: update postgresql source to localhost

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@172.20.10.3:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
